9a974900cc8af4e36158b71d43c638143e391654
CSSkelton <codysskelton@gmail.com>
2023-11-27 04:52:02 -0500

    header/footer templates and landing page

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # portfolio
+
+Bellevue University WEB 330 Personal Portfolio Project
+
+<h1>WEB 330 - Enterprise JavaScript II</h1>
+
+<h2>Contributors</h2>
+<ul>
+    <li>Cody Skelton</li>
+    <li>Professor Krasso</li>
+</ul>
---
+++ header.js
@@ -0,0 +1,27 @@
+class Header extends HTMLElement {
+    
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <h1>Cody Skelton - Web Developer</h1>
+            </header>
+
+            <nav>
+                <ul>
+                    <li><a href="index.html">Landing</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="resume.html">Resume</a></li>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="diagrams.html">DB Diagrams</a></li>
+                    <li><a href="api.html">API Unit Tests</a><li>
+                </ul>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("header-component", Header);
