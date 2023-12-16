6ecff044324d510fab72613674c006b60d40dbca
CSSkelton <codysskelton@gmail.com>
2023-12-15 21:45:32 -0500

    week 9 update

--- header.js
+++ header.js
@@ -6,20 +6,21 @@ class Header extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <header>
+            <div class="header">
                 <h1>Cody Skelton - Web Developer</h1>
-            </header>
+            
 
-            <nav>
-                <ul>
-                    <li><a href="index.html">Landing</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="resume.html">Resume</a></li>
-                    <li><a href="projects.html">Projects</a></li>
-                    <li><a href="db.html">DB Diagrams</a></li>
-                    <li><a href="api.html">API Unit Tests</a></li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li><a href="index.html">Landing</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="resume.html">Resume</a></li>
+                        <li><a href="projects.html">Projects</a></li>
+                        <li><a href="db.html">DB Diagrams</a></li>
+                        <li><a href="api.html">API Unit Tests</a></li>
+                    </ul>
+                </nav>
+            </div>
         `;
     }
 }
